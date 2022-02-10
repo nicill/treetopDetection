b96c29e8bc1a0d28c37d2825143334dbe9d9edbe
x <y>
2022-02-10 11:41:14 +0900

    params Leo code

--- Leo_TD.py
+++ Leo_TD.py
@@ -7,21 +7,21 @@ os.environ["OPENCV_IO_MAX_IMAGE_PIXELS"] = pow(2,40).__str__()
 import sys
 
 
-def execute_algorithm(image,min_hieght,result_file,result_image):
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+def execute_algorithm(image,min_hieght,result_file,result_image,size,its):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(size,size))
     time_start = time.perf_counter()
     ret,thresh = cv2.threshold(image,min_hieght,255,cv2.THRESH_TOZERO)
-    im_erode = cv2.erode(thresh,kernel,iterations=1)
+    im_erode = cv2.erode(thresh,kernel,iterations=its)
 
     norm_original = cv2.normalize(image,dst=None,alpha=0,beta=255,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_8U)
     normalized = cv2.normalize(im_erode,dst=None,alpha=0,beta=255,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_8U)
     im_show = cv2.cvtColor(norm_original,cv2.COLOR_GRAY2RGBA)
-    blurred = cv2.GaussianBlur(im_erode,(5,5),0)
+    blurred = cv2.GaussianBlur(im_erode,(size,size),0)
 
     im_dilate = cv2.dilate(blurred,kernel,iterations=2)
 
     im_comp = cv2.compare(blurred,im_dilate,cv2.CMP_GE)
-    im_erode = cv2.erode(blurred,kernel,iterations=2)
+    im_erode = cv2.erode(blurred,kernel,iterations=2*its)
     im_plateu = cv2.compare(blurred,im_erode,cv2.CMP_GT)
     im_and = cv2.bitwise_and(im_comp,im_plateu)
 
@@ -66,12 +66,14 @@ if __name__ == '__main__':
 
     path_image = sys.argv[1]
     result_image = sys.argv[2]
-    min_tree_height = 2 #set minimun tree height
     result_file = sys.argv[3] # set name of results file
+    min_tree_height = int(sys.argv[4]) #set minimun tree height
+    kernelSize=int(sys.argv[5])
+    morphIterations=int(sys.argv[6])
     #If needed can copy lines 66,68,69 and add more images, needs to change the name of the variables
     original_image = cv2.imread(path_image,cv2.IMREAD_UNCHANGED)
 
-    execute_algorithm(original_image,min_tree_height,result_file,result_image)
+    execute_algorithm(original_image,min_tree_height,result_file,result_image,kernelSize,morphIterations)
 
     #shows images with opencv viewer
     #cv2.imshow("Result Image",treetops)
